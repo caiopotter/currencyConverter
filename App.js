1f4bdf04930daa8b3afff335cfb0c6334a5d9b30
caiopotter <limapotter@gmail.com>
2022-01-27 17:41:37 -0300

    Blank app commit

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, DrawerLayoutAndroid } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Header, Icon, Text, Button } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
+
+  const drawer = useRef(null);
+
+  const docsNavigate = () => {
+    Linking.openURL(`https://reactnativeelements.com/docs/${props.view}`);
+  };
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
+    </View>
+  );
+
   return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={'left'}
+      renderNavigationView={navigationView}
+    >
+    <SafeAreaProvider>
+      <Header
+      backgroundImageStyle={{}}
+      barStyle="default"
+      centerComponent={{
+        text: "MY TITLE",
+        style: { color: "#fff" }
+      }}
+      centerContainerStyle={{}}
+      leftComponent={{ icon: "menu", color: "#fff", onPress: () => {drawer.current.openDrawer()} }}
+      leftContainerStyle={{}}
+      placement="left"
+      rightContainerStyle={{}}
+      statusBarProps={{}}
+    />
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+                title={'React Native Elements'}
+                containerStyle={{
+                  width: 200,
+                  marginHorizontal: 50,
+                  marginVertical: 10,
+                }}
+              />
+      <TouchableOpacity onPress={docsNavigate}>
+                <Icon name="description" color="white" />
+              </TouchableOpacity>
     </View>
+    </SafeAreaProvider>
+    </DrawerLayoutAndroid>
   );
 }
 
@@ -17,4 +72,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: "center"
+  },
+  navigationContainer: {
+    backgroundColor: "#ecf0f1"
+  }
 });
+
+export default App;
